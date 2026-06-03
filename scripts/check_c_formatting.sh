4b77393a2cedd2a9c8ec01d2a6307d1b3ffdebce
Krisztian Szilvasi <34309983+kr-t@users.noreply.github.com>
2026-06-03 18:20:31 +0200

    feat(scripts): add linter

---
+++ scripts/check_c_formatting.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+ARGUMENT="--dry-run"
+
+if [ $# -eq 1 ]; then
+    if [ "$1" = "-f" ]; then
+        echo "Fixing files in place if necessary"
+        ARGUMENT="-i"
+    else
+        echo "Invalid option: $1"
+        exit 1
+    fi
+fi
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+SRC_DIR="$ROOT_DIR/src"
+
+echo "Checking C formatting."
+
+find . -type f '(' -name '*.c' -o -name '*.h' ')' \
+    '(' -path './src/*' -o -path './tests/*' ')' \
+    ! -name '*/build/*' \
+    -print0 | \
+    xargs -0 -n1 clang-format ${ARGUMENT} -Werror
