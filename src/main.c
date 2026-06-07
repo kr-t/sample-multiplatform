0141e22598ca93e8478ad24541b1afcaf97f84c2
Krisztian Szilvasi <34309983+kr-t@users.noreply.github.com>
2026-06-07 14:52:05 +0200

    feat(libs): incorporate common into main

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// #define USE_COMMON
+
+#ifdef USE_COMMON
+#include <sample/common.h>
+#endif
+
 int main(void)
 {
 	printf("Hello World\n");
+#ifdef USE_COMMON
+	printf("%d + %d = %d\n", 2, 3, sum(2, 3));
+#endif
 	return 0;
 }
