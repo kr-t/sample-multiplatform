dd18db89f7b28148f77e315aad462fc72d643e8c
Krisztian Szilvasi <34309983+kr-t@users.noreply.github.com>
2026-07-07 09:15:03 +0200

    chore(format): run c formatting for tests

--- tests/system/common.c
+++ tests/system/common.c
@@ -8,52 +8,49 @@
 
 #include <sample/common.h>
 
-
 void setUp(void)
 {
-
 }
 
 void tearDown(void)
 {
-
 }
 
 void test_common_basic(void)
 {
-    TEST_ASSERT_EQUAL_INT(5, sum(2, 3));
-    TEST_ASSERT_NOT_EQUAL_INT(6, sum(2, 3));
+	TEST_ASSERT_EQUAL_INT(5, sum(2, 3));
+	TEST_ASSERT_NOT_EQUAL_INT(6, sum(2, 3));
 }
 
 void test_common_negative(void)
 {
-    TEST_ASSERT_EQUAL_INT(-1, sum(2, -3));
-    TEST_ASSERT_EQUAL_INT(-5, sum(-2, -3));
+	TEST_ASSERT_EQUAL_INT(-1, sum(2, -3));
+	TEST_ASSERT_EQUAL_INT(-5, sum(-2, -3));
 }
 
 void test_common_zero(void)
 {
-    TEST_ASSERT_EQUAL_INT(2, sum(2, 0));
-    TEST_ASSERT_EQUAL_INT(0, sum(0, 0));
+	TEST_ASSERT_EQUAL_INT(2, sum(2, 0));
+	TEST_ASSERT_EQUAL_INT(0, sum(0, 0));
 }
 
 void test_common_large_values(void)
 {
-    // Behavior depends on int overflow, but we can still test expected wrap-around
-    int a = 2000000000;
-    int b = 1000000000;
-    int result = sum(a, b);
+	// Behavior depends on int overflow, but we can still test expected wrap-around
+	int a = 2000000000;
+	int b = 1000000000;
+	int result = sum(a, b);
 
-    // Just check that the function returns *something* consistent with C integer overflow
-    TEST_ASSERT_EQUAL_INT(a + b, result);
+	// Just check that the function returns *something* consistent with C integer overflow
+	TEST_ASSERT_EQUAL_INT(a + b, result);
 }
 
 int main(void)
 {
-    UNITY_BEGIN();
-    RUN_TEST(test_common_basic);
-    RUN_TEST(test_common_negative);
-    RUN_TEST(test_common_zero);
-    RUN_TEST(test_common_large_values);
-    return UNITY_END();
+	UNITY_BEGIN();
+	RUN_TEST(test_common_basic);
+	RUN_TEST(test_common_negative);
+	RUN_TEST(test_common_zero);
+	RUN_TEST(test_common_large_values);
+	return UNITY_END();
 }
